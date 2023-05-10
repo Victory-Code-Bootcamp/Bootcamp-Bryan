4fc2fbc1a2f76401da1c212b0209d7bb5a684c6a
Bryan Suarez <bryan.suarez.sec@gmail.com>
2023-05-10 11:55:35 -0700

    Take Home finished

---
+++ responsivevictory/day7/takeHomeChallenge/js/script.js
@@ -0,0 +1,16 @@
+let childrenNum = 4;
+let partnerName = "Anastasia";
+let geoLocation = "Texas";
+let jobTitle = "Red Teamer";
+
+console.log(
+  `You will be a ${jobTitle} in ${geoLocation}, and married to ${partnerName} with ${childrenNum}`
+);
+
+let birthYear = 1997;
+let futureYear = 2026;
+let agedYear = 2026 - 1997;
+
+console.log(
+  `I will either be a ${agedYear - 1} or ${agedYear} in ${futureYear}`
+);
